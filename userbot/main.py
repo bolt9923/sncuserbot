ece02e9f4f7d83eb9c8a39d9d4ebbcc57974924f
bolt9923 <150222423+bolt9923@users.noreply.github.com>
2026-05-03 11:23:50 -0700

    Update main.py

--- userbot/main.py
+++ userbot/main.py
@@ -1,1 +1,15 @@
+from telethon import TelegramClient
+from userbot.config import API_ID, API_HASH, SESSION
 
+client = TelegramClient(SESSION, API_ID, API_HASH)
+
+# Import handlers
+from userbot.handlers import tagging, controls, clone  # noqa
+
+def start():
+    client.start()
+    print("🚀 Userbot Running...")
+    client.run_until_disconnected()
+
+if __name__ == "__main__":
+    start()
